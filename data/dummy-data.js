f3a10612c590c69cb06c9aeadf6d8dfaec6ed317
SwamynathanProfessional <swamynathan.bommaganty@gmail.com>
2021-08-18 13:24:42 +0530

    part3

---
+++ data/dummy-data.js
@@ -0,0 +1,9 @@
+import Category from '../Models/category';
+
+export const CATEGORIES = [
+  new Category(
+    'c1',
+    'Mango',
+    'https://static.wixstatic.com/media/2cd43b_5bfcda5812cf4f6cbbc883b4c99dde25~mv2.png/v1/fill/w_320,h_293,q_90/2cd43b_5bfcda5812cf4f6cbbc883b4c99dde25~mv2.png',
+  ),
+];
